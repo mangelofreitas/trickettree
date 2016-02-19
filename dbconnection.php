71610612a412e24b321dc774d51547fdc5846dab
Miguel Ângelo Simões Freitas <mfreitas@student.dei.uc.pt>
2016-02-19 18:45:25 +0000

    Init

---
+++ dbconnection.php
@@ -0,0 +1,17 @@
+<?php
+    if(isset($_SESSION['conn']))
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "teste";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password,$dbname);
+        
+        if ($conn->ping()) 
+        {
+            $_SESSION['conn'] = $conn;
+        }
+    }
+    
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	//verify the connection to database
+    include('dbconnection.php');
+?>
+
+<?php 
+	//for header in the top
+	include('header.php');
+?>
+
+<?php
+	//for footer in the bottom
+	include('footer.php');
+?>
+
