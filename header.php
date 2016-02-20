ca8383e7e8b519da2742489947230a45b23f18ce
Telmo Neves <telmo.jsneves@gmail.com>
2016-02-20 16:33:13 +0000

    treecreation.php

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
                     </form>
                 </div>
                 <li class="active"><a href="communitytree.html">Community Tree</a></li>
-                <li class="active"><a href="treecreation.html">Create Tree</a></li>
+                <li class="active"><a href="treecreation.php">Create Tree</a></li>
                 <li class="login"><?php if(!isset($_SESSION['loggedin']))
                                         {
                                             include('login.php');
