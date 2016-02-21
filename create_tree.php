af89fed84e400be8a3386775d1b4dfaead1dfda0
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2016-02-21 01:28:22 +0000

    Final

--- create_tree.php
+++ create_tree.php
@@ -6,11 +6,10 @@
     $dbname = "shifttree";
     $user=$_SESSION['id'];
     $conn = new mysqli($servername, $username, $password, $dbname);
-    $treeName = isset($_POST['treeName'])?$_POST['treeName'] : null;
     $rootName = isset($_POST['rootName'])?$_POST['rootName'] : null;
     $rootDescription = isset($_POST['rootDescription'])?$_POST['rootDescription'] : null;
     $insert_root_sql = "INSERT INTO NODE (NAME, DESCRIPTION, ID_USER) VALUES ('".$rootName."', '".$rootDescription."',".$user.");";
     $result = $conn->query($insert_root_sql);
     echo $result;
-    //header("Location:index.php");
+    header("Location:index.php");
 ?>
